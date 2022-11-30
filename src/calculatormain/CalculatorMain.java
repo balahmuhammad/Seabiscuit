7216f534138e74077bf1b880ff3c8669b27934ae
Muhammad <balah.muhammad@lyon.edu>
2022-11-30 10:45:21 -0600

    First commit

---
+++ src/calculatormain/CalculatorMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculatormain;
+
+/**
+ *
+ * @author balah.muhammad
+ */
+public class CalculatorMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calc g = new Calc();
+        g.setSize(500,525);
+        g.setVisible(true);
+    }
+    
+}
